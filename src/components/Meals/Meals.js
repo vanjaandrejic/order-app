dad68809d8ab6a052ac09553ed90b1b9cf01c1c1
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-11-02 13:32:06 +0100

    Meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailibleMeals from "./AvailibleMeals";
+
+export default function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <AvailibleMeals />
+    </>
+  );
+}
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from './MealsSummary.module.css'
+
+export default function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Choose your favorite meal from our board selection of availible meals
+        and enjoy a delicous lunch or dinner at home
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, jusit-in-time
+        and of course by experienced chefs!
+      </p>
+    </section>
+  );
+}
