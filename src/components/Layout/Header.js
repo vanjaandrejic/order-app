cc706853f733b93ab0a8a5c896efeb98eee8f199
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-11-03 13:59:14 +0100

    cart button state onClick

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
     <>
         <header className={classes.header}>
             <h1>MealsOrder</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='A table full of delicious food!' />
